075bf6a82a668a689fb1bc0a228329f799002df9
EssieYin <myin08@syr.edu>
2022-05-10 03:39:21 -0400

    1

---
+++ project/Myscript.js
@@ -0,0 +1,58 @@
+function InfoOff(){ 
+    document.getElementById('easonchan').style.display='none';
+    document.getElementById('lesliecheung').style.display='none';
+    document.getElementById('anitamui').style.display='none';
+    document.getElementById('jackycheung').style.display='none';
+    document.getElementById('mariamyeung').style.display='none';
+    document.getElementById('joeyyung').style.display='none';
+    document.getElementById('leokoo').style.display='none';
+    document.getElementById('hinscheung').style.display='none';
+    document.getElementById('easonchaninfo').style.display='none';
+    document.getElementById('lesliecheunginfo').style.display='none';
+    document.getElementById('anitamuiinfo').style.display='none';
+    document.getElementById('jackycheunginfo').style.display='none';
+    document.getElementById('mariamyeungnfo').style.display='none';
+    document.getElementById('joeyyunginfo').style.display='none';
+    document.getElementById('leokooinfo').style.display='none';
+    document.getElementById('hinscheunginfo').style.display='none';
+    }
+    
+function EasonChanOn(){
+    document.getElementById('easonchan').style.display = 'block';
+    document.getElementById('easonchaninfo').style.display = 'block';
+    }
+
+function LeslieCheungOn(){
+    document.getElementById('lesliecheung').style.display = 'block';
+    document.getElementById('lesliecheunginfo').style.display = 'block';
+    }
+
+function AnitaMuiOn(){
+    document.getElementById('anitamui').style.display = 'block';
+    document.getElementById('anitamuiinfo').style.display = 'block';
+    }
+
+function JackyCheungOn(){
+    document.getElementById('jackycheung').style.display = 'block';
+    document.getElementById('jackycheunginfo').style.display = 'block';
+    }
+
+function MariamYeungOn(){
+    document.getElementById('mariamyeung').style.display = 'block';
+    document.getElementById('mariamyeunginfo').style.display = 'block';
+    }
+
+function JoeyYungOn(){
+    document.getElementById('joeyyung').style.display = 'block';
+    document.getElementById('joeyyunginfo').style.display = 'block';
+    }
+
+function LeoKooOn(){
+    document.getElementById('leokoo').style.display = 'block';
+    document.getElementById('leokooinfo').style.display = 'block';
+    }
+
+function HinsCheungOn(){
+    document.getElementById('hinschung').style.display = 'block';
+    document.getElementById('hinschunginfo').style.display = 'block';
+    }
